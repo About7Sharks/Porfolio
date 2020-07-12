38e040237691c6fb2f84d4acc9b89137ae097f0b
Zachary <zacarlin@gmail.com>
2020-07-12 11:23:14 -0400

    lets try agian

--- src/components/journal/Post.js
+++ src/components/journal/Post.js
@@ -1,24 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
 import ReactMarkdown from 'react-markdown'
 import matter from 'gray-matter'
 
 export default function BlogPostViewer(props){
     let [responseData, setResponseData] =useState('');
-    console.log(props.match.params.id)
-    const fetchArticle= React.useCallback(()=>{
-        fetch('https://raw.githubusercontent.com/About7Sharks/Markdown/master/'+props.match.params.id+'.md')
-          .then(res=>{ 
-            res.text().then(data=>{
-            setResponseData(matter(data))
-            })
-            })
-    },[])
-    useEffect(() => {fetchArticle()},[fetchArticle]);
+    const [state] = useState({ id: props.match.params.id});
+
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/About7Sharks/Markdown/master/'+state.id+'.md')
+          .then(res=>{ res.text().then(data=>{ setResponseData(matter(data)) })})
+    });
 
      return(
         <div className="content">
-            {/* <ReactMarkdown className="article" linkTarget='_blank' source={responseData.content || 'Nothing'}/> */}
-            <p>{props.match.params.id}</p>
+            <ReactMarkdown className="article" linkTarget='_blank' source={responseData.content || 'Nothing'}/>
         </div>
           
      )
