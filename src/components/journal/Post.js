d9d7dac9f7dc03dc824d2b2e904ea80e23670750
Zachary <zacarlin@gmail.com>
2020-07-12 12:22:19 -0400

    added if statement to stop subsequent request

--- src/components/journal/Post.js
+++ src/components/journal/Post.js
@@ -5,16 +5,19 @@ import matter from 'gray-matter'
 export default function BlogPostViewer(props){
     let [responseData, setResponseData] =useState('');
     const [state] = useState({ id: props.match.params.id});
+    console.log(responseData.content)
+    useEffect(() => { 
+        if(responseData.content===undefined){
 
-    useEffect(() => {
         fetch('https://raw.githubusercontent.com/About7Sharks/Markdown/master/'+state.id+'.md')
-          .then(res=>{ res.text().then(data=>{ setResponseData(matter(data)) })})
-    });
+        .then(res=>{ res.text().then(data=>{ setResponseData(matter(data)) })})
+        }
+    })
+
 
      return(
         <div className="content">
             <ReactMarkdown className="article" linkTarget='_blank' source={responseData.content || 'Nothing'}/>
         </div>
-          
      )
   }
