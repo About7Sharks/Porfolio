9b29e1c54a7bbad3c7589131e51e8becd495e42d
Zachary <zacarlin@gmail.com>
2020-09-14 14:14:52 -0400

    lighthouse

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,18 +6,18 @@ import Emoji from './util/emoji'
 function Home (){
 
 
-  useEffect(() => {
-    // Update the document title using the browser API
-    // document.title = `You clicked 1 times`;
-    // fetch('https://res.cloudinary.com/sockrates/image/upload/v1599758087/compressUnder1MB_jkmyys.jpg').then(body=>{
-    //   let cat=body.body.getReader()
-    //   console.log(cat)
-    // })
-    // console.log(document.getElementsByClassName('App')[0])
-    setTimeout(()=>{
-      document.getElementsByClassName('App')[0].style.backgroundImage = "url('https://res.cloudinary.com/sockrates/image/upload/v1599758087/compressUnder1MB_jkmyys.webp')";
-    },1)
-  })
+  // useEffect(() => {
+  //   // Update the document title using the browser API
+  //   // document.title = `You clicked 1 times`;
+  //   // fetch('https://res.cloudinary.com/sockrates/image/upload/v1599758087/compressUnder1MB_jkmyys.jpg').then(body=>{
+  //   //   let cat=body.body.getReader()
+  //   //   console.log(cat)
+  //   // })
+  //   // console.log(document.getElementsByClassName('App')[0])
+  //   setTimeout(()=>{
+  //     document.getElementsByClassName('App')[0].style.backgroundImage = "url('https://res.cloudinary.com/sockrates/image/upload/c_scale,h_1378,q_73/v1599758087/compressUnder1MB_jkmyys.webp')";
+  //   },1)
+  // })
 
   return(
       <div className="App">
