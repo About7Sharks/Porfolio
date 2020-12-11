7d5568334b42fe5b44bb821b0543badf43153fc0
sokil <sokil@localhost.localdomain>
2020-12-11 11:34:39 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-import React, { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
-import './components/myscss.scss';
-import BlogPosts from './components/journal/AllPosts'
-import BlogPost from './components/journal/Post'
-const Projects = lazy(() => import("./components/projects/projects.js"))
-const Drawer = lazy(() => import("./components/navigation/drawer.js"))
-const Footer = lazy(() => import('./components/navigation/footer'))
-const Home = lazy(() => import('./components/Home.js'))
-const About = lazy(() => import('./components/about/about'))
-const BackBTN = lazy(() => import('./components/navigation/backbutton'))
+import React, { lazy, Suspense } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import "./components/myscss.scss";
+import BlogPosts from "./components/journal/AllPosts";
+import BlogPost from "./components/journal/Post";
+const Projects = lazy(() => import("./components/projects/projects.js"));
+const Drawer = lazy(() => import("./components/navigation/drawer.js"));
+const Footer = lazy(() => import("./components/navigation/footer"));
+const Home = lazy(() => import("./components/Home.js"));
+const About = lazy(() => import("./components/about/about"));
+const BackBTN = lazy(() => import("./components/navigation/backbutton"));
 
 const renderLoader = () => <p>Loading ...</p>;
 
-
 function App() {
-    return (
-        <Suspense fallback={renderLoader()}>
-            <Router>
-                <Drawer />
-                <BackBTN id="backBTN" />
-                <Switch>
-                    <Route exact path={["/", "/home"]} component={Home} />
-                    <Route exact path="/journal" component={BlogPosts} />
-                    <Route exact path='/journal/:id' component={BlogPost} />
-                    <Route exact path="/about" component={About} />
-                    <Route exact path="/projects" component={Projects} />
-                    <Route path='*' render={() => <Redirect to='/' />} />
-                </Switch>
-                <Footer />
-            </Router>
-        </Suspense>
-    )
+  return (
+    <Suspense fallback={renderLoader()}>
+      <Router>
+        <Drawer />
+        <BackBTN id="backBTN" />
+        <Switch>
+          <Route exact path={["/", "/home"]} component={Home} />
+          <Route exact path="/journal" component={BlogPosts} />
+          <Route path="/journal/:id" component={BlogPost} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route path="*" render={() => <Redirect to="/" />} />
+        </Switch>
+        <Footer />
+      </Router>
+    </Suspense>
+  );
 }
 
 export default App;
--- src/components/journal/Post.js
+++ src/components/journal/Post.js
@@ -1,40 +1,41 @@
 import React, { useState, useEffect } from "react";
-import ReactMarkdown from "react-markdown";
-import matter from "gray-matter";
+// import ReactMarkdown from "react-markdown";
+// import matter from "gray-matter";
 // import "./posts.scss";
-import { useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
 export default function BlogPostViewer(props) {
-  const { state } = useLocation(); // if defined is, contains post data
-  const [postData, setPost] = useState("");
-  const [Id] = useState(props.match.params.id);
-  //fetches post from git
-  const getPost = async () => {
-    let post = await (
-      await fetch(
-        "https://raw.githubusercontent.com/About7Sharks/Markdown/master/" +
-          Id +
-          ".md"
-      )
-    ).text();
-    setPost(matter(post));
-  };
-  useEffect(() => {
-    //if direct link to post; fetch data
-    if (state === undefined && postData === "") {
-      getPost();
-      console.log("state undefined and no previous data");
-    } else if (state !== undefined) {
-      console.log("state defined, skipping fetch since data already loaded");
-      setPost({ content: state.content });
-    }
-    //scroll to top of page
-    window.scrollTo(0, 0);
-  }, []);
+  // const { state } = useLocation(); // if defined is, contains post data
+  // const [postData, setPost] = useState("");
+  // const [Id] = useState(props.match.params.id);
+  // //fetches post from git
+  // const getPost = async () => {
+  //   let post = await (
+  //     await fetch(
+  //       "https://raw.githubusercontent.com/About7Sharks/Markdown/master/" +
+  //         Id +
+  //         ".md"
+  //     )
+  //   ).text();
+  //   setPost(matter(post));
+  // };
+  // useEffect(() => {
+  //   //if direct link to post; fetch data
+  //   if (state === undefined && postData === "") {
+  //     getPost();
+  //     console.log("state undefined and no previous data");
+  //   } else if (state !== undefined) {
+  //     console.log("state defined, skipping fetch since data already loaded");
+  //     setPost({ content: state.content });
+  //   }
+  //   //scroll to top of page
+  //   window.scrollTo(0, 0);
+  // }, []);
   return (
-    <ReactMarkdown
-      className={`${props.match.params.id} article`}
-      linkTarget="_blank"
-      source={postData.content || "Nothing"}
-    />
+    // <ReactMarkdown
+    //   className={`${props.match.params.id} article`}
+    //   linkTarget="_blank"
+    //   source={postData.content || "Nothing"}
+    // />
+    <div>df</div>
   );
 }
