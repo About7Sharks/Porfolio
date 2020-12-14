571d984130cb5cc17f36e1896cb63afc013df405
Zachary <zacarlin@gmail.com>
2020-12-13 19:53:02 -0500

    testing without redirects

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,15 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js"
+);
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
 //enables google analytics offline support
-workbox.googleAnalytics.initialize(); 
-console.log('service-worker.js has just been ran');
+workbox.googleAnalytics.initialize();
+console.log("service-worker.js has just been ran");
 
 //add listener for skip waiting event from registerServiceWorker.js
-self.addEventListener('message', event => {
-  if (event.data === 'skipWaiting') {
-    console.log('skip waiting invoked')
+self.addEventListener("message", (event) => {
+  if (event.data === "skipWaiting") {
+    console.log("skip waiting invoked");
     self.skipWaiting();
   }
 });
