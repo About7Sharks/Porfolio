77d0ab0dd293084e78e13b7cdbdc4eb0f2a4a3b0
Zachary <zacarlin@gmail.com>
2021-01-10 21:13:30 -0500

    added tnew loading component, as to knowledge fixed journal thrwwing error

--- src/components/util/BeforeLoad.tsx
+++ src/components/util/BeforeLoad.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-
+import "./beforeLoad.scss";
 interface Props {}
 
 export const BeforeLoad = (props: Props) => {
   return (
-    <div style={{ height: "100vh", width: "100vw", background: "white" }}>
-      <p>Loading ...</p>
+    <div id="load">
+      <div>G</div>
+      <div>N</div>
+      <div>I</div>
+      <div>D</div>
+      <div>A</div>
+      <div>O</div>
+      <div>L</div>
     </div>
   );
 };
