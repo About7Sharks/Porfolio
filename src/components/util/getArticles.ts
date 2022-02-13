794b151406c3ee5ea3d0fee2109bd50bee6dd932
Zach <zacarlin@gmail.com>
2022-02-12 23:56:07 -0500

    minor clean up

--- src/components/util/getArticles.ts
+++ src/components/util/getArticles.ts
@@ -1,33 +1,19 @@
 import matter from "gray-matter";
 
 async function getArticles() {
-  let data = await (
-    await fetch(
-      "https://api.github.com/repos/About7Sharks/Markdown/git/trees/main?recursive=1"
-    )
-  ).json();
+  let data = await (await fetch("https://api.github.com/repos/About7Sharks/Markdown/git/trees/main?recursive=1")).json();
    data.tree=data.tree.filter((path : any)=>{
-      return path.path.includes('md')
+    return path.path.includes('md')
    })
    console.log(data.tree)
   let articlesContent =( await Promise.all(
     data.tree.map(async (article: any) => {
-      let art = matter(
-        await (
-          await fetch(
-            `https://raw.githubusercontent.com/About7Sharks/Markdown/main/${article.path}`
-          )
-        ).text()
-      );
+      let art = matter(await ( await fetch(`https://raw.githubusercontent.com/About7Sharks/Markdown/main/${article.path}`)).text() );
       if (Object.keys(art.data).length === 0)return
-      art = { 
-        ...art,
-        ...art.data
-      };
+      art = {...art, ...art.data};
       return art;
     })
     )).filter(Boolean);
-    console.log(articlesContent)
   localStorage.setItem("data", JSON.stringify(articlesContent));
 
   return articlesContent;
