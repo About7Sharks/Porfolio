2359fe3ae51e87354dd2c280a8de419e0c4a70ee
Zachary Carlin <zach@Zacharys-MacBook-Pro-2.local>
2022-05-26 21:03:11 -0400

    Added comment.

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,3 +1,4 @@
+// Learned from https://medium.com/swlh/import-components-in-react-like-a-pro-b1340cb76a1b
 export { WarningMessage} from './WarningMessage';
 export { spinAnimation, spinAnimationV2 } from './SpinAnimation.js';
 export {default as Picker} from './Picker'
